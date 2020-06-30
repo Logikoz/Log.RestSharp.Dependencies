6b020e831778ccb490414c140c3520f9d2f802c0
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-06-29 21:21:04 -0500

    Criando testes e atualizando restsharp [1.1.2]

---
+++ Dependencies.Tests/Program.cs
@@ -0,0 +1,33 @@
+﻿using Dependencies.Services;
+
+using RestSharp;
+
+using System;
+using System.Threading.Tasks;
+
+namespace Dependencies.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Tests();
+		}
+
+		private static async void Tests()
+		{
+			await SendRequestTaskAsync();
+
+			async Task<IRestResponse> SendRequestTaskAsync()
+			{
+				RequestService requestService = new RequestService
+				{
+					Method = Method.POST,
+					URL = $"http://192.168.1.6:5000/api",
+					URN = "auth/signup"
+				};
+				return await requestService.ExecuteTaskAsync();
+			}
+		}
+	}
+}
--- Dependencies/Services/RequestService.cs
+++ Dependencies/Services/RequestService.cs
@@ -8,68 +8,68 @@ using System.Threading.Tasks;
 
 namespace Dependencies.Services
 {
-    public class RequestService
-    {
-        private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
+	public class RequestService
+	{
+		private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
 
-        public Method Method { private get; set; }
-        public object Body { private get; set; }
+		public Method Method { private get; set; }
+		public object Body { private get; set; }
 
-        /// <summary>
-        /// Use: Parameters.Add(paramName, paramValue);
-        /// </summary>
-        public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
+		/// <summary>
+		/// Use: Parameters.Add(paramName, paramValue);
+		/// </summary>
+		public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
 
-        public IAuthenticator Authenticator { private get; set; }
+		public IAuthenticator Authenticator { private get; set; }
 
-        /// <summary>
-        /// <para>without '/' before.</para>
-        /// <para>Example: images/test.png</para>
-        /// <para>Result by joining the protocol, url and parameters: <see href="https://Logikoz.net/images/test.png?widht=100&amp;weight=100"/></para>
-        /// </summary>
-        public string URN { private get; set; }
+		/// <summary>
+		/// <para>without '/' before.</para>
+		/// <para>Example: images/test.png</para>
+		/// <para>Result by joining the protocol, url and parameters: <see href="https://Logikoz.net/images/test.png?widht=100&amp;weight=100"/></para>
+		/// </summary>
+		public string URN { private get; set; }
 
-        /// <summary>
-        /// Only the URL, without the protocol.
-        /// <para>Example: logikoz.net</para>
-        /// </summary>
-        public string URL { private get; set; }
+		/// <summary>
+		/// Only the URL, without the protocol.
+		/// <para>Example: https://logikoz.net</para>
+		/// </summary>
+		public string URL { private get; set; }
 
-        public string UserAgent { private get; set; }
+		public string UserAgent { private get; set; }
 
-        /// <summary>
-        /// Default values:
-        /// <para>name: Content-Type</para>
-        /// <para>value: application/json</para>
-        /// </summary>
-        public (string name, string value) Header { private get; set; } = ("Content-Type", "application/json");
+		/// <summary>
+		/// Default values:
+		/// <para>name: Content-Type</para>
+		/// <para>value: application/json</para>
+		/// </summary>
+		public (string name, string value) Header { private get; set; } = ("Content-Type", "application/json");
 
-        public async Task<IRestResponse> ExecuteTaskAsync()
-        {
-            var request = new RestRequest(Method);
-            request.AddHeader(Header.name, Header.value);
+		public async Task<IRestResponse> ExecuteTaskAsync()
+		{
+			var request = new RestRequest(Method);
+			request.AddHeader(Header.name, Header.value);
 
-            if (Body != null)
-                request.AddJsonBody(Body);
+			if (Body != null)
+				request.AddJsonBody(Body);
 
-            var client = new RestClient($"{URL}/{URN}{(Parameters.Count > 0 ? GetParameters() : string.Empty)}")
-            {
-                UserAgent = UserAgent
-            };
+			var client = new RestClient($"{URL}/{URN}{(Parameters.Count > 0 ? GetParameters() : string.Empty)}")
+			{
+				UserAgent = UserAgent
+			};
 
-            if (Authenticator != default)
-                client.Authenticator = Authenticator;
+			if (Authenticator != default)
+				client.Authenticator = Authenticator;
 
-            return await client.ExecuteAsync(request, _cancellationToken.Token);
-        }
+			return await client.ExecuteAsync(request, _cancellationToken.Token);
+		}
 
-        public void CancelRequest() => _cancellationToken.Cancel();
+		public void CancelRequest() => _cancellationToken.Cancel();
 
-        private string GetParameters()
-        {
-            string parameters = "?";
-            Parameters.ToList().ForEach(param => parameters += $"{param.Key}={param.Value}{(Parameters.Count > 1 ? "&" : string.Empty)}");
-            return parameters;
-        }
-    }
+		private string GetParameters()
+		{
+			string parameters = "?";
+			Parameters.ToList().ForEach(param => parameters += $"{param.Key}={param.Value}{(Parameters.Count > 1 ? "&" : string.Empty)}");
+			return parameters;
+		}
+	}
 }
