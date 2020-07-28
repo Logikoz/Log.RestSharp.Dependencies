f4cf6629e3ca3e8832787075c748e4177e6e3cec
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-07-27 22:22:44 -0500

    Criando testes do metodo GET

---
+++ Dependencies.Tests/GetRequestTest.cs
@@ -0,0 +1,46 @@
+﻿using Dependencies.Services;
+
+using System.Net;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Dependencies.Tests
+{
+	public class GetRequestTest
+	{
+		[Theory]
+		[InlineData("1")]
+		[InlineData("2")]
+		[InlineData("3")]
+		public async Task GetPostsAndReturnOk(string value)
+		{
+			var response = await new RequestService
+			{
+				URL = "http://jsonplaceholder.typicode.com",
+				URN = $"posts/{value}",
+				Method = RestSharp.Method.GET
+			}
+			.ExecuteTaskAsync();
+
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+
+		[Theory]
+		[InlineData("345k34l5k")]
+		[InlineData("23k4l2k34")]
+		[InlineData("fasd8f9asd8")]
+		public async Task GetPostsAndReturnNotFound(string value)
+		{
+			var response = await new RequestService
+			{
+				URL = "http://jsonplaceholder.typicode.com",
+				URN = $"posts/{value}",
+				Method = RestSharp.Method.GET
+			}
+			.ExecuteTaskAsync();
+
+			Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+		}
+	}
+}
