39e8946d256b0509a5bc006c6aa2f4feef5ccd38
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-07-30 22:52:49 -0500

    Criando test para enviar requisiçao http post e retornar true

---
+++ RestSharp.Dependencies.Tests/PostRequestTest.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp.Dependencies.Services;
+
+using System.Net;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace RestSharp.Dependencies.Tests
+{
+	public class PostRequestTest
+	{
+		[Theory]
+		[InlineData("{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }")]
+		[InlineData("{ \"title\": \"foo2\", \"body\": \"bar2\", \"userId\": 2 }")]
+		public async Task SendPostAndReturnData(string json)
+		{
+			var response = await new RequestService
+			{
+				URL = "http://jsonplaceholder.typicode.com",
+				URN = $"posts",
+				Method = Method.POST,
+				Body = json
+			}.ExecuteTaskAsync();
+
+			Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+		}
+	}
+}
