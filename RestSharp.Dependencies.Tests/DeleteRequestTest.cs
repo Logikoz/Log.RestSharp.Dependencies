d22b2e867559bf727ba7ad7e4266107c93e2fc2a
Ruan Carlos CS <ruancarlos14@hotmail.com>
2020-07-30 23:07:29 -0500

    implementando test para http delete e retorna 200

---
+++ RestSharp.Dependencies.Tests/DeleteRequestTest.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp.Dependencies.Services;
+
+using System.Net;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace RestSharp.Dependencies.Tests
+{
+	public class DeleteRequestTest
+	{
+		[Theory]
+		[InlineData("1")]
+		[InlineData("2")]
+		[InlineData("3")]
+		[InlineData("4")]
+		public async Task DeletePostAndReturnEqual200(string value)
+		{
+			var response = await new RequestService
+			{
+				URL = "http://jsonplaceholder.typicode.com",
+				URN = $"posts/{value}",
+				Method = Method.DELETE
+			}.ExecuteTaskAsync();
+
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+	}
+}
